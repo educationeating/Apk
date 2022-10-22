e1b8abddf81ef18d2859d64328d475e26f81428e
educationeating <79743110+educationeating@users.noreply.github.com>
2022-10-22 15:24:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function opv(){
+    document.getElementById("pv").style.display ="flex";
+}
+
+function cpv(){
+    document.getElementById("pv").style.display ="none";
+}
+
+function download(){
+    window.location.href ="./education-eating.apk";
+}
